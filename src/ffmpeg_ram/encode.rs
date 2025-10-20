87fea077f65e1684d368b4cb1ab49ab53728751d
21pages <sunboeasy@gmail.com>
2025-10-20 17:39:55 +0800

    enable h264_videotoolbox
    Signed-off-by: 21pages <sunboeasy@gmail.com>

--- src/ffmpeg_ram/encode.rs
+++ src/ffmpeg_ram/encode.rs
@@ -266,16 +266,16 @@ impl Encoder {
 
         #[cfg(target_os = "macos")]
         {
-            let (_h264, h265, _, _) = crate::common::get_video_toolbox_codec_support();
+            let (h264, h265, _, _) = crate::common::get_video_toolbox_codec_support();
             // h264 encode failed too often, not AV_CODEC_CAP_HARDWARE
-            // if h264 {
-            //     codecs.push(CodecInfo {
-            //         name: "h264_videotoolbox".to_owned(),
-            //         format: H264,
-            //         priority: Priority::Best as _,
-            //         ..Default::default()
-            //     });
-            // }
+            if h264 {
+                codecs.push(CodecInfo {
+                    name: "h264_videotoolbox".to_owned(),
+                    format: H264,
+                    priority: Priority::Best as _,
+                    ..Default::default()
+                });
+            }
             if h265 {
                 codecs.push(CodecInfo {
                     name: "hevc_videotoolbox".to_owned(),
